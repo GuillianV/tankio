df6f497193b00150c15c43fa56068327902661b2
GuillianV <guillianvibert90@gmail.com>
2021-10-14 15:04:13 +0200

    add projectileManager in gameManager

--- Assets/Scripts/Projectile/Bullet.cs
+++ Assets/Scripts/Projectile/Bullet.cs
@@ -3,34 +3,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bullet : Projectile
+public class Bullet : MonoBehaviour
 {
     public BulletData Data;
-
+    private Animator m_animator;
+    private Rigidbody2D m_rigidBody;
+    private PolygonCollider2D m_polyCollider;
     public void LoadData(BulletData _data)
     {
         Data = _data;
     }
-    
-    public override void Fire()
+
+    private void Awake()
+    {
+        m_animator = GetComponent<Animator>();
+        m_rigidBody = GetComponent<Rigidbody2D>();
+        m_polyCollider = GetComponent<PolygonCollider2D>();
+    }
+
+    private void Start()
     {
         gameObject.SetActive(true);
-        
     }
 
-    public override void Collided()
+    private void OnCollisionEnter2D(Collision2D other)
     {
-        Debug.Log("bullet collided");
-        Destroy();
+        m_animator.SetTrigger("IsDead");
+        
+        Destroy(m_rigidBody);
+        Destroy(m_polyCollider);
     }
 
-    public override void Destroy()
+    public void EndDestroyAnim()
     {
-        
         Destroy(gameObject);
     }
-
    
+    
+
+
+
+
+
+
+    
+    
 }
 
 
