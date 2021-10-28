f69c4070c71d4eb65eb1321fa4c250da5a366443
GuillianV <guillianvibert90@gmail.com>
2021-10-28 13:12:15 +0200

    add chunk light and rotate

--- Assets/Scripts/Sprites/RotateZero.cs
+++ Assets/Scripts/Sprites/RotateZero.cs
@@ -7,12 +7,7 @@ public class RotateZero : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.transform.rotation =  Quaternion.Euler(0f, 0f, 0f);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
