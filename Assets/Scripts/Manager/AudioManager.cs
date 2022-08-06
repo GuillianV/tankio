23767848de452bf7d08a22be3f16ae1c759f3e10
lsmnt <leasimonet56@gmail.com>
2022-08-06 16:25:55 +0200

    add playlist and new sounds

--- Assets/Scripts/Manager/AudioManager.cs
+++ Assets/Scripts/Manager/AudioManager.cs
@@ -4,9 +4,17 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
+
 public class AudioManager : MonoBehaviour
 {
 
+
+    //Playlist part
+    public PlayList playList;
+    private AudioSource PlaylistSource;
+    private List<AudioClip> playlistClipsToPlay = new List<AudioClip>();
+    private int audioIndex = 0;
+    private bool playlistStart = false;
     public List<Sound> sounds;
 
     // Start is called before the first frame update
@@ -44,4 +52,54 @@ public class AudioManager : MonoBehaviour
        }
       
     }
+
+
+
+    public void StartPlaylist()
+    {
+        PlaylistSource = gameObject.AddComponent<AudioSource>();
+        PlaylistSource.volume = playList.volume;
+        PlaylistSource.pitch = (playList.pitch + 1);
+        if (playList.isRandom)
+        {
+            List<AudioClip> clipPartial = new List<AudioClip>();
+            playList.clips?.ForEach(c =>
+            {
+                clipPartial.Add(c);
+            });
+            for (int i = playList.clips.Count; i > 0; i--)
+            {
+                int rand = UnityEngine.Random.Range(0, clipPartial.Count);
+                playlistClipsToPlay.Add(clipPartial[rand]);
+                clipPartial.Remove(clipPartial[rand]);
+            }
+        }
+        else
+        {
+            playlistClipsToPlay = playList.clips;
+        }
+
+        playlistStart = true;
+    }
+    
+    
+    void Update () {
+        if (!PlaylistSource.isPlaying )
+        {
+            if (playlistStart && playlistClipsToPlay.Count > 0)
+            {
+                PlaylistSource.clip = playlistClipsToPlay[audioIndex];
+                PlaylistSource.Play();
+                audioIndex++;
+                if (audioIndex >= playlistClipsToPlay.Count )
+                {
+                    audioIndex = 0;
+                }
+            }
+            
+          
+        }
+           
+    }
+    
 }
---
+++ Assets/Scripts/Sound/PlayList.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "Sound/Playlist")]
+public class PlayList : ScriptableObject
+{
+    public List<AudioClip> clips;
+    [Range(0,1)]
+    public float volume = 0.5f;
+    [Range(-1,1)]
+    public float pitch = 0;
+    [HideInInspector]
+    public AudioSource source;
+    public bool isRandom;
+}
