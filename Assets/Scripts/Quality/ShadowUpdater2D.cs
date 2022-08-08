a471415ef274222e81a452eec2486223930aab6e
Guillian VIBERT-VALLET <guillian.vibert-vallet@boondooa.com>
2022-08-08 17:05:35 +0200

    added disable animator and shadows when camera out

---
+++ Assets/Scripts/Quality/ShadowUpdater2D.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+[RequireComponent(typeof(ShadowCaster2D))]
+[RequireComponent(typeof(SpriteRenderer))]
+public class ShadowUpdater2D : MonoBehaviour
+{
+    private ShadowCaster2D _shadows;
+    private SpriteRenderer _renderer;
+    private Animator _animator;
+   
+    void Start()
+    {
+        _shadows = GetComponent<ShadowCaster2D>();
+        _renderer = GetComponent<SpriteRenderer>();
+        _animator = GetComponent<Animator>();
+        
+        
+        if (!_renderer.isVisible)
+        {
+            ToggleGameObject(false);
+        }
+
+     
+        
+    }
+
+    private void OnBecameInvisible()
+    {
+        ToggleGameObject(false);
+    }
+
+    private void OnBecameVisible()
+    {
+        ToggleGameObject(true);
+    }
+
+    public void ToggleGameObject(bool value)
+    {
+        //_renderer.enabled = value;
+        _shadows.enabled = value;
+        
+        if (_animator)
+        {
+            _animator.enabled = value;
+        }
+    }
+    
+}
